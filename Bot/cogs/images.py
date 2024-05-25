aadbea3f950735e9c7cf53fd65cf32322eb08903
Hazrat Ummar Shaikh <hazratummar9@gmail.com>
2024-05-25 22:29:42 +0530

    added search images command

--- Bot/cogs/images.py
+++ Bot/cogs/images.py
@@ -49,5 +49,41 @@ class Images(commands.Cog):
                 "Sorry, there was an error processing your request. Please try again later."
             )
 
+    @app_commands.command(name= "images_search", description="search a images")
+    async def image_search(self, interaction : discord.Interaction, q:str):
+        key = os.getenv("pixabay_API")
+        response = requests.get(f"https://pixabay.com/api/?key={key}&q={q}")
+
+        data = response.json()
+
+        images = [item for item in data["hits"] if "previewURL" in item]
+        if not images:
+            await interaction.response.send_message("No Images Found.")
+            return
+        # preview_url = [item["previewURL"] for item in images]
+
+        random_images = random.choice(images)
+
+        embed = discord.Embed(
+            title=f"Your search result for {q}",
+            description= "",
+            color= discord.Color.dark_embed()
+        )
+        embed.add_field(name="Likes", value=random_images["likes"])
+        embed.add_field(name="Downloads", value=random_images["downloads"])
+        embed.add_field(name="Views", value=random_images["views"])
+        embed.set_image(url= random_images["largeImageURL"])
+        embed.set_author(name=random_images["user"], icon_url=random_images["userImageURL"])
+
+        button1 = discord.ui.Button(
+            label="Open Image", 
+            url=random_images["pageURL"],
+            style= discord.ButtonStyle.link
+        )
+        view = discord.ui.View()
+        view.add_item(button1)
+
+        await interaction.response.send_message(embed= embed, view= view)
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(Images(bot))
